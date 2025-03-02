2d92a1c69ed30fba02d4aac9dc9c31d3b355df64
Vladislav Kozello <vkozello2005@gmail.com>
2025-03-02 20:43:22 +0300

    final marks in HAKATON

---
+++ IGI/LR2/geometric_lib/main.py
@@ -0,0 +1,9 @@
+import circle
+import square
+
+var = int(input("Enter length of square side: "))
+print("Square area = ", square.area(var))
+print("Square perimeter = ", square.perimeter(var))
+var = int(input("Enter length of circle radius: "))
+print("Circle area = ", circle.area(var))
+print("Circle perimeter = ", circle.perimeter(var))
