a00b58ccf3ad73f578a40cc2bcbf27b26a4f1c45
Vladislav Kozello <vkozello2005@gmail.com>
2025-04-03 18:40:41 +0300

    some commit

--- IGI/LR4/Task1/task1.py
+++ IGI/LR4/Task1/task1.py
@@ -1,2 +1,25 @@
+import csv
+import pickle
+
+
 class task1:
+    def write_csv(self, items):
+        with open("data.csv", mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=items[0].keys())
+            writer.writeheader()
+            writer.writerows(items)
+
+    def write_pickle(self, items):
+        with open("data.pkl", "wb") as file:
+            pickle.dump(items, file)
+
+    def read_csv(self):
+        with open("data.csv", mode="r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            return [row for row in reader]
+
+    def read_pickle(self):
+        with open("data.pkl", "rb") as file:
+            return pickle.load(file)
+
     pass
